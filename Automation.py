06cf79189a27c832c5657042cc59c656a0de5687
Qi <coki020412@gmail.com>
2026-09-25 11:11:11 +0200

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -36,7 +36,7 @@ div[data-testid="stBaseButton-primary"] {
 
 # === 2. Daten laden ===
 EXCEL_PFAD_MAIN = "Expert Automation Final v02.xlsx"
-EXCEL_PFAD_STOCK = "Stock & Supply Report 20260917.xlsx"
+EXCEL_PFAD_STOCK = "Stock & Supply Report 20260925.xlsx"
 
 @st.cache_data
 def lade_daten(datei_pfad_main, datei_pfad_stock):
